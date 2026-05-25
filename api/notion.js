0a29467b348785b6cda0f034987fbb55c0ebeb1f
joannatapioca <joannajozviak@gmail.com>
2026-05-25 21:07:18 +0200

    Create notion.js

---
+++ api/notion.js
@@ -0,0 +1,21 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  if (req.method === 'OPTIONS') return res.status(200).end()
+
+  const response = await fetch(
+    `https://api.notion.com/v1/databases/${process.env.NOTION_DB_ID}/query`,
+    {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${process.env.NOTION_API_KEY}`,
+        'Notion-Version': '2022-06-28',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ page_size: 60 })
+    }
+  )
+  const data = await response.json()
+  res.status(200).json(data)
+}
